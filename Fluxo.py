7a404f6f8d900b194566bf9154f337b8652997e8
LUCAS DA MATA PRADO <lucas.mp7@puccampinas.edu.br>
2026-05-25 09:08:41 -0300

    Update Fluxo.py

--- Fluxo.py
+++ Fluxo.py
@@ -1,25 +1,28 @@
 # ==== PROTÓTIPO DO PROJETO ====
 def menu_principal():
-    opcao = 0
-    while opcao != 3:
+    opcao = "0"
+    while opcao != "4":
         print("\n ==== Fluxo Norte ====")
         print("1 - Pedidos")
         print("2 - Listar Entregadores")
         print("3 - Gerar relatório")
-        print("0 - Sair do sistema")
+        print("4 - Sair do sistema")
         opcao = input("Escolha uma opção:")
         match opcao:
-            case 1:
+            case "1":
                 menu_dos_pedidos()
-            case 2:
+            case "2":
                 menu_dos_entregadores()
-            case 3:
+            case "3":
                 menu_dos_relatorios()
-            case _:
+            case "4":
                 print("Saindo do sistema...")
+            case _:
+                print("opção inválida")
 def menu_dos_pedidos():
     pass
 def menu_dos_entregadores():
     pass
 def menu_dos_relatorios():
     pass
+menu_principal()
