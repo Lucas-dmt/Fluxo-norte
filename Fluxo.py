7e5223eb0707f7db7cafe89bd5cfb9bf64c94b18
LUCAS DA MATA PRADO <lucas.mp7@puccampinas.edu.br>
2026-05-25 11:07:46 -0300

    criação dos menus

--- Fluxo.py
+++ Fluxo.py
@@ -1,4 +1,4 @@
-# ==== PROTÓTIPO DO PROJETO ====
+
 def menu_principal():
     opcao = "0"
     while opcao != "4":
@@ -20,9 +20,67 @@ def menu_principal():
             case _:
                 print("opção inválida")
 def menu_dos_pedidos():
-    pass
+    opcao = "0"
+    while opcao != "4":
+        print("\n==== PEDIDOS ====")
+        print("1 - Fazer pedido")
+        print("2 - Editar pedido")
+        print("3 - Remover pedido")
+        print("4 - Voltar para o menu principal")
+        opcao = input("Escolha uma opção:")
+        match opcao:
+            case "1":
+                print("Work in progress...")
+            case "2":
+                print("Work in progress")
+            case "3":
+                print("Work in progress")
+            case "4":
+                menu_principal()       
 def menu_dos_entregadores():
-    pass
+    opcao = "0"
+    while opcao != "5":
+        print("\n==== ENTREGADORES ==== ")
+        print("1 - Cadastrar entregador")
+        print("2 - Listar entregadores")
+        print("3 - Associar pedido")
+        print("4 - Remover associação")
+        print("5 - Voltar")
+        opcao = input("Escolha uma opção:")
+        match opcao:
+            case "1":
+                print("Work in progress...")
+            case "2":
+                print("Work in progress...")
+            case "3":
+                print("Work in progress...")
+            case "4":
+                print("Work in progress...")
+            case "5":
+                menu_principal()
 def menu_dos_relatorios():
-    pass
+    opcao = "0"
+    while opcao != "5":
+        print("\n==== RELATÓRIO ====")
+        print("1 - Total de pedidos")
+        print("2 - Pedidos por status")
+        print("3 - Pedidos de Alta prioridade")
+        print("4 - Entregador com mais entregas")
+        print("5 - Voltar")
+        opcao = input("Escolha uma opção:")
+        match opcao:
+            case "1":
+                print("Work in progress...")
+            case "2":
+                print("Work in progress...")
+            case "3":
+                print("Work in progress...")
+            case "4":
+                print("Work in progress...")
+            case "5":
+                menu_principal()
 menu_principal()
+
+pedidos = {}
+entregadores ={}
+
